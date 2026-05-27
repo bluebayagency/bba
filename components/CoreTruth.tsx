b1efcc552ed458c933b3b2a643ea8f3dcd6f4cfe
Veronica Perez <bluebayagencyllc@gmail.com>
2026-05-27 09:04:47 -0700

    chore: update content

--- components/CoreTruth.tsx
+++ components/CoreTruth.tsx
@@ -51,8 +51,7 @@ export default function CoreTruth() {
               </p>
             </ScrollReveal>
 
-            <ScrollReveal delay={3}>
-            </ScrollReveal>
+
           </div>
 
         </div>
